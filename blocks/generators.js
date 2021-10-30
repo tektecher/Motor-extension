c4e711fd0655eab0ca3b73b54a1dc8990881f6a6
amrharb <65994648+amrharb@users.noreply.github.com>
2021-10-30 23:54:36 +0200

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,9 +1,86 @@
-Blockly.Python['servo'] = function(block) {
-  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-  Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
+Blockly.Python["pin_motor_right_stop"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${false})\n`;
+  return code;
+};
 
-  var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
-  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=50).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
+Blockly.Python["pin_motor_right_forward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18;
+  var code = `Pin(${pin1}, Pin.OUT).value(${true})\nPin(${pin2}, Pin.OUT).value(${false})\n`;
+  return code;
+};
+
+Blockly.Python["pin_motor_right_backward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${true})\n`;
+  return code;
+};
+
+Blockly.Python["pin_motor_left_stop"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 19,
+    pin2 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${false})\n`;
+  return code;
+};
+
+Blockly.Python["pin_motor_left_forward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 19,
+    pin2 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${true})\nPin(${pin2}, Pin.OUT).value(${false})\n`;
+  return code;
+};
+
+Blockly.Python["pin_motor_left_backward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 19,
+    pin2 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${true})\n`;
+  return code;
+};
+
+Blockly.Python["pin_robot_stop"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18,
+    pin3 = 19,
+    pin4 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${false})\nPin(${pin3}, Pin.OUT).value(${false})\nPin(${pin4}, Pin.OUT).value(${false})\n`;
+  return code;
+};
+
+Blockly.Python["pin_robot_forward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18,
+    pin3 = 19,
+    pin4 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${true})\nPin(${pin2}, Pin.OUT).value(${false})\nPin(${pin3}, Pin.OUT).value(${false})\nPin(${pin4}, Pin.OUT).value(${true})\n`;
+  return code;
+};
+
+Blockly.Python["pin_robot_backward"] = function (block) {
+  Blockly.Python.definitions_["from_machine_import_pin"] =
+    "from machine import Pin";
+  var pin1 = 14,
+    pin2 = 18,
+    pin3 = 19,
+    pin4 = 23;
+  var code = `Pin(${pin1}, Pin.OUT).value(${false})\nPin(${pin2}, Pin.OUT).value(${true})\nPin(${pin3}, Pin.OUT).value(${true})\nPin(${pin4}, Pin.OUT).value(${false})\n`;
   return code;
 };
