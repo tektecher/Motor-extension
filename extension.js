33001e3dd20afd30ac2f3390ab250dd14d58315f
amrharb <65994648+amrharb@users.noreply.github.com>
2021-10-31 05:44:12 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,27 +1,18 @@
 ({
-    name: "Servo", // Category Name
-    description: "Set angle to Servo motor",
-    author: "microBlock",
-    category: "Device Control",
-    version: "1.0.0",
-    icon: "/static/icon.png", // Category icon
-    color: "#0271D9", // Category color (recommend some blocks color)
-    blocks: [ // Blocks in Category
-        {
-            xml: `
-                <block type="servo">
-                    <value name="pin">
-                        <shadow type="math_number">
-                            <field name="NUM">14</field>
-                        </shadow>
-                    </value>
-                    <value name="angle">
-                        <shadow type="math_number">
-                            <field name="NUM">90</field>
-                        </shadow>
-                    </value>
+  name: "DC Motor", // Category Name
+  description: "Set pins to DC motor",
+  author: "Tektech",
+  category: "Motor Control",
+  version: "1.0.0",
+  icon: "/static/icon/jpg", // Category icon
+  color: "#0271D9", // Category color (recommend some blocks color)
+  blocks: [
+    // Blocks in Category
+    {
+      xml: `
+                <block type="DC_Motor">
                 </block>
-            `
-        }
-    ]
+            `,
+    },
+  ],
 });
